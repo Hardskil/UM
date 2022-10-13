879af9a5caab584991dd2929c031eb4a70a580b3
Bekzod <88338975+Hardskil@users.noreply.github.com>
2022-10-13 12:37:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+document.querySelector(`#btn`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m`).style.display = 'block'
+    document.querySelector(`#btn_2`).style.display = 'block'
+    document.querySelector(`#btn`).style.display = 'none'
+})
+document.querySelector('#btn_2').addEventListener('click',function(e){
+    document.querySelector(`#sec_m`).style.display = 'none'
+    document.querySelector(`#btn_2`).style.display = 'none'
+    document.querySelector(`#btn`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-2`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-2`).style.display = 'block'
+    document.querySelector(`#btn_2-2`).style.display = 'block'
+    document.querySelector(`#btn-2`).style.display = 'none'
+})
+document.querySelector('#btn_2-2').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-2`).style.display = 'none'
+    document.querySelector(`#btn_2-2`).style.display = 'none'
+    document.querySelector(`#btn-2`).style.display = 'block'
+})
+
+
+document.querySelector(`#btn-3`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-3`).style.display = 'block'
+    document.querySelector(`#btn_2-3`).style.display = 'block'
+    document.querySelector(`#btn-3`).style.display = 'none'
+})
+document.querySelector('#btn_2-3').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-3`).style.display = 'none'
+    document.querySelector(`#btn_2-3`).style.display = 'none'
+    document.querySelector(`#btn-3`).style.display = 'block'
+})
+
+
+
+
+
+
+
+document.querySelector(`#btn-5`).addEventListener('click',function(e) {
+    document.querySelector(`#sec_m-5`).style.display = 'block'
+    document.querySelector(`#btn_2-5`).style.display = 'block'
+    document.querySelector(`#btn-5`).style.display = 'none'
+})
+document.querySelector('#btn_2-5').addEventListener('click',function(e){
+    document.querySelector(`#sec_m-5`).style.display = 'none'
+    document.querySelector(`#btn_2-5`).style.display = 'none'
+    document.querySelector(`#btn-5`).style.display = 'block'
+})
